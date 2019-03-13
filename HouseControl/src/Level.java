2384c9afd48e0c238e9331749904e60e1bf15df7
Eddo0702 <eduardoperez.eddo@gmail.com>
2019-03-13 13:03:16 -0500

    Added javadoc

--- HouseControl/src/Level.java
+++ HouseControl/src/Level.java
@@ -18,60 +18,81 @@ public class Level {
 		this.rooms = new ArrayList<Room>();
 	}
 	
+	/**
+	 * Adds a room to the array labeled rooms.
+	 * @param	room	The room to add.
+	 */
 	public void addRoom(Room room) {
 		rooms.add(room);
 	}
+	
+	/**
+	 * removes a room from the array labeled rooms.
+	 * @param	room	The room to remove.
+	 */
+	public boolean removeRoom(Room room) {
+		return rooms.remove(room);
+	}
 
 	/**
-	 * @return the roomCounter
+	 * The current amount of rooms in the level.
+	 * @return	Integer	The size of the array of devices.
 	 */
 	public int getRoomCounter() {
 		return rooms.size();
 	}
 
 	/**
-	 * @return the rooms
+	 * @return	rooms	The array containing all the rooms in the level.
 	 */
 	public ArrayList<Room> getRooms() {
 		return rooms;
 	}
 
 	/**
-	 * @param rooms the rooms to set
+	 * Replaces the rooms in this level with a new list of rooms.
+	 * @param rooms The new list of rooms.
 	 */
 	public void setRooms(ArrayList<Room> rooms) {
 		this.rooms = rooms;
 	}
 
 	/**
-	 * @return the name
+	 * Returns the name of the level. Said name is often used as an identifier so the level can be tracked.
+	 * @return	name	The name of the level.
 	 */
 	public String getName() {
 		return name;
 	}
 
 	/**
-	 * @param name the name to set
+	 * Sets the name of the level. Said name is often used as an identifier so the level can be tracked.
+	 * @param	name	The name of the level.
 	 */
 	public void setName(String name) {
 		this.name = name;
 	}
 
-	public int searchRoom(Room otherRoom) {
+	/**
+	 * Loops trough the array to find the given room.
+	 * @param	room	The room we want to find in the room.
+	 * @return	position	The position in the array of the room. If the room was no found returns -1.
+	 */
+	public int searchRoom(Room room) {
 		int position = 0;
 		for (int index = 0; index < rooms.size(); index++) {
-			if (rooms.get(index).equals(otherRoom)) {
+			if (rooms.get(index).equals(room)) {
 				position = index;
 				break;
 			}
 		}
 		return position;
 	}
-
-	public boolean removeRoom(Room room) {
-		return rooms.remove(room);
-	}
 	
+	/**
+	 * The current amount of devices turned on in the level.
+	 * @return	devicesOn	The number of devices where the status is True.
+	 */
 	public int countDevicesOn() {
 		int devicesOn = 0;
 		int numberOfRooms = getRoomCounter();
@@ -81,6 +102,11 @@ public class Level {
 		return devicesOn;
 	}
 	
+	/**
+	 * The current amount of devices that have a certain name.
+	 * @param	name	The name of the device to count.
+	 * @return	deviceCount	The number of devices found which have the name specified as parameter.
+	 */
 	public int countSpecificDevices(String name) {
 		int deviceCount = 0;
 		int numberOfRooms = getRoomCounter();
@@ -90,6 +116,11 @@ public class Level {
 		return deviceCount;
 	}
 	
+	/**
+	 * Switch the status from On to Off and vice versa to devices that have a certain name.
+	 * @param	name	The name of the device to toggle.
+	 * @return	toggled	True if at least one device was toggled False if no device was toggled.
+	 */
 	public boolean toggleSpecificDevice (String name) {
 		boolean toogled = false;
 		int numberOfRooms = getRoomCounter();
@@ -101,6 +132,10 @@ public class Level {
 		return toogled;
 	}
 
+	/**
+	 * Returns the Information of the level. This is often used for display purposes.
+	 * @return	String	A String containing the name of the room followed by a list of the rooms in the level with their respective devices.
+	 */
 	public String toString() {
 		String output = "";
 		output = output + name + "\n";
@@ -110,18 +145,29 @@ public class Level {
 		return output;
 	}
 
+	/**
+	 * Turns off All the devices in the level
+	 */
 	public void switchOffAllRooms() {
 		for (int index = 0; index < getRoomCounter(); index++) {
 			rooms.get(index).switchOffAllDevices();
 		}
 	}
 
+	/**
+	 * Turns on All the devices in the level
+	 */
 	public void switchOnAllRooms() {
 		for (int index = 0; index < getRoomCounter(); index++) {
 			rooms.get(index).switchOnAllDevices();
 		}
 	}
 
+	/**
+	 * Turns off All the devices in a given room of this level
+	 * @param	room	The room which its devices will turn off.
+	 * @return	boolean	True if the room given room exist in this level False if the room was not found in this level.
+	 */
 	public boolean switchOffRoom(Room room) {
 		int index;
 		index = this.searchRoom(room);
@@ -132,6 +178,11 @@ public class Level {
 		return false;
 	}
 	
+	/**
+	 * Turns on All the devices in a given room of this level
+	 * @param	room	The room which its devices will turn on.
+	 * @return	boolean	True if the given room exist in this level False if the room was not found in this level.
+	 */
 	public boolean switchOnRoom(Room room) {
 		int index;
 		index = this.searchRoom(room);
@@ -142,6 +193,13 @@ public class Level {
 		return false; 
 	}
 
+	/**
+	 * Turns on or off a given device in a given room within this level
+	 * @param	room	The room which contains the device that will be switched.
+	 * @param	device	The device which will be switched.
+	 * @param	setOn	the status of the device True for On and False for Off.
+	 * @return	boolean	True if the given device exist in this level False if the level was not found in this level.
+	 */
 	public boolean levelSwitchDevice(Room room, Device device, boolean setOn) {
 		Device roomDevice = getRoomDevice(room, device);
 		if (roomDevice ==  null)
@@ -154,6 +212,13 @@ public class Level {
 		
 		return true;
 	}
+	
+	/**
+	 * Search for a given device within a given room
+	 * @param	room	The room which contains the device.
+	 * @param	device	The device the be found.
+	 * @return	Device	The device contained in the given room. NUll if neither the room nor the device where found.
+	 */
 	
 	private Device getRoomDevice(Room room, Device device) {
 		int lRoom = this.searchRoom(room);
@@ -168,6 +233,11 @@ public class Level {
 
 		return null;
 	}
+	
+	/**
+	 * Turn off all the devices that have a certain name.
+	 * @param	name	The name of the device to turn off.
+	 */
 
 	public void switchOffDevicesByName(String name) {
 		int numberOfRooms = rooms.size();
@@ -175,6 +245,12 @@ public class Level {
 			rooms.get(i).swithcOffDevicesByName(name);
 		}
 	}
+	
+	/**
+	 * Compares the level to another object.
+	 * @param	obj		The object to to compare to.
+	 * @return	boolean	The result of the comparison. True if both objects are equal. False if they are not.
+	 */
 
 	public boolean equals(Object obj) {
 		if (obj instanceof Level && obj != null) {
