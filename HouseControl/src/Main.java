74925ec2cf6f8101908414712f16321bfe205400
Eddo0702 <eduardoperez.eddo@gmail.com>
2019-03-13 13:14:16 -0500

    Format Corrections

--- HouseControl/src/Main.java
+++ HouseControl/src/Main.java
@@ -1,12 +1,4 @@
 
-import java.util.ArrayList;
-
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
 /**
  *
  * @author edgar.cambranes
@@ -90,11 +82,6 @@ public class Main {
         room.addDevice(new Device ("Jonatan","samsung","J5",true));
         room.addDevice(new Device("Jessica", "Huaweii", "P9LITE", true));
         */
-        
-        
-        
-        
-        
     }
     
 }
