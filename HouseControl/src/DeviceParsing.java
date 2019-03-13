c81316e00eb10b08b51d1281d551b3fa55a14b42
Eddo0702 <eduardoperez.eddo@gmail.com>
2019-03-13 11:58:38 -0500

    Added javadoc

--- HouseControl/src/DeviceParsing.java
+++ HouseControl/src/DeviceParsing.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 /**
  *
- * @author edgar.cambranes
+ * @author eduardo.perez
  */
 public class DeviceParsing {
     ArrayList<Device> devices;
@@ -13,29 +13,31 @@ public class DeviceParsing {
 
     }
     
+    /**
+	 * Reads the content of Devices.text and sets it to content
+	 */
     public void getString(){
         content = new FileReader("Devices.txt").getContentFile();
-    
     }
     
+    /**
+	 * Separates content by each instance of a comma.
+	 * @return	parsing	The content converted to an array 	
+	 */
     public String[] parsingContent(){
         
         String[] parsing= content.split(",");
         return parsing;
     }
     
+    /**
+	 * Creates devices and sets them to the list of devices called devices 
+	 * @param	parsing	The list of device attributes each type of attribute repeats every three elements of the array,
+	 * 					first name, then brand, and finally model.  
+	 */
     public void setDevices(String[] parsing){
         for(int i = 0; i<parsing.length; i=i+2){
             devices.add(new Device(parsing[i], parsing[i+1], parsing[i+2], true));
         }
     }
-    
-    
-    public static void main(String ar[]){
-        DeviceParsing dp = new DeviceParsing();
-        dp.getString();
-        dp.parsingContent();
-    
-    }
-    
 }
